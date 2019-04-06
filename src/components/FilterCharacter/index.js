ce1e1f60a527195f49936577fb0544bb0a42b4c0
ZloyLeva <zloyleva@gmail.com>
2019-04-06 12:44:17 +0300

    modify Filter Characters

--- src/components/FilterCharacter/index.js
+++ src/components/FilterCharacter/index.js
@@ -1,64 +1,94 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './style.css';
 
-export default (props) => {
+export default class FilterCharcter extends Component {
 
-    const {filterCharacters} = props;
+    state = {
+        name: "",
+        gender: "",
+    };
 
-    return (
-        <div className="Filter__Wrapper">
-            <h2 className="Filter__Wrapper_Title">Search characters</h2>
-            <form action="" className="Form__Wrapper_container" onSubmit={(event) => {submitFilterHandler(event,filterCharacters)}}>
-                <div className="Input__Wrapper">
-                    <label htmlFor="search_name" className="Label__InputText">Name:</label>
-                    <input id="search_name" type="text" className="Input__SearchText" placeholder="Enter name of character"/>
-                </div>
-                <div className="Input__Wrapper">
-                    <div className="Label__For_Radio">gender:</div>
-                    <label className="Label__InputText">Male
-                        <input type="radio" className="Input__Radio_select" value="male" name="gender"/>
-                    </label>
-                    <label className="Label__InputText">Female
-                        <input type="radio" className="Input__Radio_select" value="female" name="gender"/>
-                    </label>
-                    <label className="Label__InputText">Genderless
-                        <input type="radio" className="Input__Radio_select" value="genderless" name="gender"/>
-                    </label>
-                    <label className="Label__InputText">Unknown
-                        <input type="radio" className="Input__Radio_select" value="unknown" name="gender"/>
-                    </label>
-                </div>
-                <button className="Btn__Filter_data">Filter</button>
-            </form>
-        </div>
-    );
-}
+    render() {
+        const {filterCharacters} = this.props;
 
-const submitFilterHandler = (e, filterCharacters) => {
-    console.log("submitFilterHandler");
-    e.preventDefault();
-
-    if(hasDataToFilter(e)){
-        const search = getSearchValue(e);
-        const gender = getRadioValue(e);
-
-        const o = {};
-
-        if(search){o.name = search}
-        if(gender){o.gender = gender}
-
-        filterCharacters(o);
+        return (
+            <div className="Filter__Wrapper">
+                <h2 className="Filter__Wrapper_Title">Search characters</h2>
+                <form action="" className="Form__Wrapper_container" onSubmit={(event) => {
+                    this.submitFilterHandler(event, filterCharacters)
+                }}>
+                    <div className="Input__Wrapper">
+                        <label htmlFor="search_name" className="Label__InputText">Name:</label>
+                        <input id="search_name" type="text" className="Input__SearchText"
+                               placeholder="Enter name of character" onInput={this.inputNameHandler}/>
+                    </div>
+                    <div className="Input__Wrapper">
+                        <div className="Label__For_Radio">gender:</div>
+                        <label className="Label__InputText">Male
+                            <input type="radio" className="Input__Radio_select" value="male"
+                                   name="gender" onChange={this.changeGenderHandler}/>
+                        </label>
+                        <label className="Label__InputText">Female
+                            <input type="radio" className="Input__Radio_select" value="female"
+                                   name="gender" onChange={this.changeGenderHandler}/>
+                        </label>
+                        <label className="Label__InputText">Genderless
+                            <input type="radio" className="Input__Radio_select" value="genderless"
+                                   name="gender" onChange={this.changeGenderHandler}/>
+                        </label>
+                        <label className="Label__InputText">Unknown
+                            <input type="radio" className="Input__Radio_select" value="unknown"
+                                   name="gender" onChange={this.changeGenderHandler}/>
+                        </label>
+                    </div>
+                    <button className="Btn__Filter_data">Filter</button>
+                </form>
+            </div>
+        );
     }
 
-};
+    changeGenderHandler = (e) => {
+        console.log(e.target.value);
+    }
 
-const getRadioValue = (e) => {
-    return [...e.target].filter(el => el.checked).length > 0 ? [...e.target].filter(el => el.checked)[0].value:""
-};
+    inputNameHandler = (e) => {
+        console.log(e.target.value);
 
-const getSearchValue = (e) => {
-    return e.target[0].value;
-};
+        this.setState({
+            name: e.target.value
+        })
+    };
 
-const hasDataToFilter = (e) => getSearchValue(e) || getRadioValue(e);
+    submitFilterHandler = (e, filterCharacters) => {
+        console.log("submitFilterHandler");
+        e.preventDefault();
+
+        if (this.hasDataToFilter(e)) {
+            const search = this.getSearchValue(e);
+            const gender = this.getRadioValue(e);
+
+            const o = {};
+
+            if (search) {
+                o.name = search
+            }
+            if (gender) {
+                o.gender = gender
+            }
+
+            filterCharacters(o);
+        }
+
+    };
+
+    getRadioValue = (e) => {
+        return [...e.target].filter(el => el.checked).length > 0 ? [...e.target].filter(el => el.checked)[0].value : ""
+    };
+
+    getSearchValue = (e) => {
+        return e.target[0].value;
+    };
+
+    hasDataToFilter = (e) => this.getSearchValue(e) || this.getRadioValue(e);
+}
